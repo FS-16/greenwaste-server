c3ee9a4283e0cc0446eb727538fc40d9aa8f6a93
koreoxy <rayssankn@gmail.com>
2023-11-22 12:54:58 +0700

    fix : index file

--- index.js
+++ index.js
@@ -4,23 +4,29 @@ import dotenv from 'dotenv';
 import userRoute from './routes/UserRoute.js';
 import cors from 'cors';
 
-dotenv.config();
-
-mongoose
-  .connect(process.env.MONGODB_URL)
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-
 const app = express();
 
-app.listen(5000, () => console.log('Server running...'));
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URL);
+
+const db = mongoose.connection;
+db.on('error', (error) => console.log(error));
+db.once('open', () => console.log('Database Connected to MongoDB'));
 
 app.use(cors());
 app.use(express.json());
 
-// API ROUTE
+app.get('/api', (req, res) => {
+  try {
+    const successMessage = 'API Response Success!';
+    res.status(200).json({ message: successMessage });
+  } catch (error) {
+    const errorMessage = 'Terjadi kesalahan dalam memproses permintaan.';
+    res.status(500).json({ error: errorMessage });
+  }
+});
+
 app.use('/api', userRoute);
+
+app.listen(5000, () => console.log('Server berjalan...'));
