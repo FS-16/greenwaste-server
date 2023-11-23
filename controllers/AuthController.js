d8161084b35b2ad9855375b88b6b3049a6faf173
koreoxy <rayssankn@gmail.com>
2023-11-23 20:18:19 +0700

    feat : add api auth-login

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,6 +1,7 @@
 import User from '../models/UserModel.js';
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 export const register = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -11,5 +12,28 @@ export const register = async (req, res, next) => {
     res.status(201).json('User Register Successfully');
   } catch (error) {
     next(errorHandler(500, 'Error User already Register'));
+  }
+};
+
+export const login = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, 'User Not Found'));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, 'Password Wrong !!'));
+
+    const token = jwt.sign({ _id: validUser._id }, process.env.JWT_KEY);
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .cookie('token_access', token, {
+        httpOnly: true,
+        // Expire date cookie
+        expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
+      })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
   }
 };
