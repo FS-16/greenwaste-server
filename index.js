af09ce167e94fdd022b009faa101bc9bdeee841a
koreoxy <rayssankn@gmail.com>
2023-11-23 13:14:21 +0700

    feat : add error handle

--- index.js
+++ index.js
@@ -41,4 +41,16 @@ app.use('/api', userRoute);
 app.use('/api', questionRoute);
 app.use('/api/auth', authRoute);
 
+// Middleware Handle Error message
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
+
 app.listen(5000, () => console.log('Server berjalan...'));
