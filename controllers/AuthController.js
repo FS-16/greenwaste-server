dd5c9c9d607652e43a17989106c21fce75ef1259
koreoxy <rayssankn@gmail.com>
2023-11-26 12:09:42 +0700

    fix: auth controller

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -5,35 +5,41 @@ import jwt from 'jsonwebtoken';
 
 export const register = async (req, res, next) => {
   const { username, email, password } = req.body;
+
   //HASH PASSWORD
   const hashedPassword = bcryptjs.hashSync(password, 10);
+
   // CREATE NEW USER
   const newUser = new User({ username, email, password: hashedPassword });
 
   try {
+    //SAVE NEW USER TO DATABASE
     await newUser.save();
-    res.status(201).json('User berhasil di daftarkan!!');
+    res.status(201).json('User created succesfully');
   } catch (error) {
-    next(errorHandler(550, 'name and email already exist'));
+    // next(errorHandler(550, 'error from the function'));
+    next(error);
   }
 };
 
 export const login = async (req, res, next) => {
   const { email, password } = req.body;
-
   try {
     const validUser = await User.findOne({ email });
-    if (!validUser) return next(errorHandler(404, 'User tidak ditemukan!'));
-
+    if (!validUser) return next(errorHandler(404, 'User not found'));
     const validPassword = bcryptjs.compareSync(password, validUser.password);
-    if (!validPassword) return next(errorHandler(401, 'Password Salah!'));
+    if (!validPassword) return next(errorHandler(401, 'Wrong Password'));
 
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_KEY);
 
-    // REMOVE PASSWORD when TO SEND CLIENT
     const { password: pass, ...rest } = validUser._doc;
 
-    res.cookie('access_token', token, { httOnly: true }).status(200).json(rest);
+    res
+      .cookie('access_token', token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
